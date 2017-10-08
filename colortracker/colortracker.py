092ecb836266e06b6ea46baa36734f7a60224fff
moz <moz@local>
2017-10-08 11:46:32 +0200

    added findPostit function

--- colortracker/colortracker.py
+++ colortracker/colortracker.py
@@ -2,6 +2,30 @@ import cv2
 import datetime
 import numpy as np
 
+
+def findPostit( img_orig, hsv_img, name, limit_low, limit_up ):
+        # pink post-it
+        #lower_pink = np.array([ 158, 62, 100])
+        #upper_pink = np.array([ 178, 112, 255])
+        mask = cv2.inRange(hsv_img, np.array(limit_low), np.array(limit_up))
+        res = cv2.bitwise_and(img_orig,img_orig, mask= mask)
+
+        contours,h = cv2.findContours(mask,1,2)
+        cnts = sorted(contours, key = cv2.contourArea, reverse = True)
+        #
+        # for cnt in contours:
+        #     approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        #     #print len(approx)
+        #     #if len(approx)==4:
+        #     #print cnt
+        #     cv2.drawContours(res,[cnt],0,(255, 255,0),-1)
+
+        cv2.drawContours(res,[cnts[0]],0,(255, 255,0),-1)
+
+        cv2.namedWindow('res_'+name, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('res_'+name, 640, 360 )
+        cv2.imshow('res_'+name,res)
+
 def process( infile ):
 
     # Begin capturing video. You can modify what video source to use with VideoCapture's argument. It's currently set
@@ -41,15 +65,8 @@ def process( infile ):
         cv2.resizeWindow('res_yellow', 640, 360 )
         cv2.imshow('res_yellow',res_yellow)
 
-        # pink post-it
-        lower_pink = np.array([ 158, 62, 100])
-        upper_pink = np.array([ 178, 112, 255])
-        mask_pink = cv2.inRange(hsv, lower_pink, upper_pink)
-        res_pink = cv2.bitwise_and(frame,frame, mask= mask_pink)
-        cv2.namedWindow('res_pink', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('res_pink', 640, 360 )
-        cv2.imshow('res_pink',res_pink)
-
+        # pink
+        findPostit( frame, hsv, 'pink', [ 158, 62, 100], [ 178, 112, 255] )
 
         # green post-it
         lower_green = np.array([  36,  50,100])

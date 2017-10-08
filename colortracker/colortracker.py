8b5ff498aafe17b606dc009a1f7190f81974a852
moz <moz@local>
2017-10-08 12:24:46 +0200

    code cleanup now highlighting is separated from detection

--- colortracker/colortracker.py
+++ colortracker/colortracker.py
@@ -3,28 +3,22 @@ import datetime
 import numpy as np
 
 
-def findPostit( img_orig, hsv_img, name, limit_low, limit_up ):
+def findPostit( hsv_img, limit_low, limit_up, threshold = 1000 ):
         # pink post-it
         #lower_pink = np.array([ 158, 62, 100])
         #upper_pink = np.array([ 178, 112, 255])
         mask = cv2.inRange(hsv_img, np.array(limit_low), np.array(limit_up))
-        res = cv2.bitwise_and(img_orig,img_orig, mask= mask)
+        #res = cv2.bitwise_and(img_orig,img_orig, mask= mask)
 
         contours,h = cv2.findContours(mask,1,2)
-        cnts = sorted(contours, key = cv2.contourArea, reverse = True)
-        #
-        # for cnt in contours:
-        #     approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        #     #print len(approx)
-        #     #if len(approx)==4:
-        #     #print cnt
-        #     cv2.drawContours(res,[cnt],0,(255, 255,0),-1)
+        contour_areas = [(c, cv2.contourArea(c)) for c in contours]
+        cnts_filtered = [c for c, area in contour_areas if area > threshold]
 
-        cv2.drawContours(res,[cnts[0]],0,(255, 255,0),-1)
+        return cnts_filtered
 
-        cv2.namedWindow('res_'+name, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('res_'+name, 640, 360 )
-        cv2.imshow('res_'+name,res)
+def highlightPostits( frame, contours, line_width = 3, line_color = (255, 255,0) ):
+        if len(contours) > 0:
+            cv2.drawContours(frame,contours, -1, line_color, line_width)
 
 def process( infile ):
 
@@ -47,35 +41,15 @@ def process( infile ):
         # Convert BGR to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # orange post-it
-        lower_orange = np.array([  0,  130,  100])
-        upper_orange = np.array([ 25,170,255])
-        mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
-        res_orange = cv2.bitwise_and(frame,frame, mask= mask_orange)
-        cv2.namedWindow('res_orange', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('res_orange', 640, 360 )
-        cv2.imshow('res_orange',res_orange)
+        pink_cnt   = findPostit( hsv, [158, 62,100], [178,112,255] )
+        green_cnt  = findPostit( hsv, [ 36, 50,100], [ 56,100,255] )
+        yellow_cnt = findPostit( hsv, [ 17, 72,100], [ 37,112,255] )
+        orange_cnt = findPostit( hsv, [  0,130,100], [ 25,170,255] )
 
-        # yellow post-it
-        lower_yellow = np.array([ 17, 72, 100])
-        upper_yellow = np.array([ 37, 112, 255])
-        mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
-        res_yellow = cv2.bitwise_and(frame,frame, mask= mask_yellow)
-        cv2.namedWindow('res_yellow', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('res_yellow', 640, 360 )
-        cv2.imshow('res_yellow',res_yellow)
-
-        # pink
-        findPostit( frame, hsv, 'pink', [ 158, 62, 100], [ 178, 112, 255] )
-
-        # green post-it
-        lower_green = np.array([  36,  50,100])
-        upper_green = np.array([ 56,100,255])
-        mask_green = cv2.inRange(hsv, lower_green, upper_green)
-        res_green = cv2.bitwise_and(frame,frame, mask= mask_green)
-        cv2.namedWindow('res_green', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('res_green', 640, 360 )
-        cv2.imshow('res_green',res_green)
+        highlightPostits( frame, pink_cnt)
+        highlightPostits( frame, green_cnt)
+        highlightPostits( frame, yellow_cnt)
+        highlightPostits( frame, orange_cnt)
 
         # Converts image to grayscale.
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
